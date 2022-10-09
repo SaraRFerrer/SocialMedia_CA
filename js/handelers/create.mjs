8482c96831f44ed8df9d299e6ffbd673ca2f946b
SaraRFerrer <sararferrer@hotmail.com>
2022-10-09 16:12:09 +0200

    working on profile page

--- js/handelers/create.mjs
+++ js/handelers/create.mjs
@@ -31,56 +31,60 @@ if (path === "/profile.html") {
     createListener()
 }
 
-function getToken() {
-    const accessToken = localStorage.getItem("token");
-    if (accessToken === null) {
-      return [];
-    } else {
-      return JSON.parse(accessToken);
-    }
-  }
-  
-  const token = getToken();
-  
-  console.log(token);
-  
-  const options = {
-    headers: {
-      Authorization: `Bearer ${token}`,
-    },
-  };
 
-const queryName = document.location.search;
-const param = new URLSearchParams(queryName);
-const name = parameters.get("name");
+const action = "/posts";
+const accessToken = load("accessToken")
+const user = localStorage.getItem("profile");
+const postContainer = document.querySelector("#profilePost");
 
-const profileFeed = document.querySelector("#profilePost");
-const test = document.querySelector(".test")
+export async function getPosts () {
+    const response = await fetch (
+        `${API_PATH_URL}${action}/?sort=created&sortOrder=desc&_author=true&_comments=true&_reactions=true`,
+        {
+            method: "get",
+            headers: {
+                "Content-Type": "application/json",
+                Authorization: `Bearer ${accessToken}`,
+        },
+            body: JSON.stringify(),
+      }
+      );
+      const json = await response.json();
+      console.log(json);
 
-const API = API_PATH_URL + action + name + "?_posts=true&sortOrder=desc";
+      const filterUserData = json.filter((author) => author.name === user);
+      const userPosts = filterUserData;
 
-async function getFeed () {
-    const response = await fetchToken(renderUrl)
-    const json = await response.json();
-       
 
-    console.log(json);
-    console.log(posts);
+    userPosts.forEach((userPosts) => {
 
-    profileFeed.innerHTML = `<div class=" container mt-4 mb-5 posts-card d-flex justify-content-center row col-md-8 feed p-2
+
+
+    postContainer.innerHTML = `<div class=" container mt-4 mb-5 posts-card d-flex justify-content-center row col-md-8 feed p-2
     bg-white border mt-2">
     <div class="d-flex flex-row justify-content-between align-items-center p-2 border-bottom">
-    <p class="d-flex justify-content-end socials">${json[i].author}</p></div>
+    <p class="d-flex justify-content-end socials">${userPosts.author}</p></div>
     <div class="d-flex flex-row justify-content-between align-items-center p-2 border-bottom">
-    <h1 class="d-flex flex-row align-items-center feed-text px-2">${json[i].title}</h1></div>
+    <h1 class="d-flex flex-row align-items-center feed-text px-2">${userPosts.title}</h1></div>
     <div class="feed-image p-2 px-3">
-    <img class="img-fluid img-responsive d-flex justify-content-end socials" src ="${json[i].media}"></img></div>
+    <img class="img-fluid img-responsive d-flex justify-content-end socials" src ="${userPosts.media}"></img></div>
     <div class="p-2 px-3">
-    <p class="d-flex justify-content-end socials">${json[i].body}</p></div>
-    <div><p>${json[i].created}</p></div>`
+    <p class="d-flex justify-content-end socials">${userPosts.body}</p></div>
+    <div><p>${userPosts.created}</p></div>`
 
-  
+
+});
+
+if (!response.ok) {
+    throw new Error ("Error/ Please try again");
 }
+console.log(userPosts);
 
- getFeed();
+console.log (postContainer);
+
+}
+  
+
+
+ getPosts();
 
--- js/posts/createPost.mjs
+++ js/posts/createPost.mjs
@@ -22,6 +22,6 @@ export async function createPost(postData) {
 
 }
 
-createPost ();
+
   
 
