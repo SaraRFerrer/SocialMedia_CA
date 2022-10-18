b7aa517518a0aadb728c32243531f566d27b46da
SaraRFerrer <91666428+SaraRFerrer@users.noreply.github.com>
2022-10-18 13:00:50 +0200

    filter sorting

--- js/posts/renderPosts.mjs
+++ js/posts/renderPosts.mjs
@@ -1,53 +1,67 @@
-import { fetchToken } from "../apiHandelings/fetchToken.mjs";
-import { API_PATH_URL } from "../constants/url.mjs";
-import { filterNewest, filterOldest } from "./filter.mjs";
+import { fetchToken } from '../apiHandelings/fetchToken.mjs';
+import { API_PATH_URL } from '../constants/url.mjs';
+import { filterNewest, filterOldest } from './filter.mjs';
+
+// Used for sorting the date, we are storing this as a global variable
+let dateSortOrder = 'ascending';
+
+// This function sets the dateSortOrder above and then renders the posts again
+// Otherwise our posts will not update (we need to render new posts each time we
+// change it)
+function setDateSortOrder(order) {
+  console.log('Posts sorting');
+  dateSortOrder = order;
+  renderPosts();
+}
+
+const oldest = document.getElementById('oldest');
+const newest = document.getElementById('newest');
 
 
-
-const posts = document.querySelector("#wrapper");
-
-
-const action ="/posts";
-const today = new Date().toISOString().split("T")[0];
-const filterContainer = document.querySelector("#filter");
+// Call the function and set the order
+oldest.addEventListener('click', () => setDateSortOrder('descending'));
+newest.addEventListener('click', () => setDateSortOrder('ascending'));
 
 
+const posts = document.querySelector('#wrapper');
 
+const action = '/posts';
+const today = new Date().toISOString().split('T')[0];
+const filterContainer = document.querySelector('#filter');
 
 export async function renderPosts() {
-    const renderUrl = `${API_PATH_URL}${action}` + "?_author=true&_comments=true&limit=100";
+  const renderUrl =
+    `${API_PATH_URL}${action}` + '?_author=true&_comments=true&limit=100';
 
-    const response = await fetchToken(renderUrl)
-    const json = await response.json();
-    //console.log(json);
+  const response = await fetchToken(renderUrl);
+  const json = await response.json();
+  //console.log(json);
 
-   
+  /**
+   * User can search the array filtering the posts.
+   * @param {string} posts
+   */
 
+  function searchFeed(json) {
+    const searchInput = document.querySelector('#search');
 
-    /**
-     * User can search the array filtering the posts.
-     * @param {string} posts 
-     */
+    searchInput.onkeyup = function (event) {
+      const filterValue = event.target.value.trim().toLowerCase();
 
-    function searchFeed (json) {
-        const searchInput = document.querySelector("#search");
-    
-        searchInput.onkeyup = function (event) {
-            
-            const filterValue = event.target.value.trim().toLowerCase();
+      const filteredFeed = json.filter(function (post) {
+        if (
+          post.title.toLowerCase().includes(filterValue) ||
+          post.body.toLowerCase().includes(filterValue)
+        ) {
+          return true;
+        }
+      });
 
-           
-            const filteredFeed = json.filter(function (post) {
-                if (post.title.toLowerCase().includes(filterValue) || post.body.toLowerCase().includes(filterValue)) {
-                    return true;
-                }
-            });
+      posts.innerHTML = '';
 
-            posts.innerHTML = "";
-
-           console.log(filteredFeed);
-           for (let i = 0; i < filteredFeed.length; i++) {
-            posts.innerHTML += `<a href="details.html?id=${filteredFeed[i].id}" <div class=" container mt-4 mb-5 posts-card d-flex justify-content-center row col-md-8 feed p-2
+      console.log(filteredFeed);
+      for (let i = 0; i < filteredFeed.length; i++) {
+        posts.innerHTML += `<a href="details.html?id=${filteredFeed[i].id}" <div class=" container mt-4 mb-5 posts-card d-flex justify-content-center row col-md-8 feed p-2
             bg-white border mt-2">
             <div class="d-flex flex-row justify-content-between align-items-center p-2 border-bottom">
             <p class="d-flex justify-content-end socials">${filteredFeed[i].author.name}</p></div>
@@ -58,58 +72,47 @@ export async function renderPosts() {
             <div class="p-2 px-3">
             <p class="d-flex justify-content-end socials">${filteredFeed[i].body}</p></div>
             <div><p>${filteredFeed[i].created}</p></div>
-            <button class="btn btn-primary waves-effect waves-light">View Post</button></a>`
-        
-         } 
-    
-           
-    
-    
-        };
-
-       
-    }
-    searchFeed(json);
-
-   
-    
+            <button class="btn btn-primary waves-effect waves-light">View Post</button></a>`;
+      }
+    };
+  }
+  searchFeed(json);
  
+  // Create a new array which we will sort
+  let postsList = [...json];
+  // Checking if we need to sort asc/desc
+  if (dateSortOrder === 'ascending') {
+    postsList = postsList.sort(function (a, b) {
+      return new Date(b.created) - new Date(a.created);
+    });
+  } else {
+    postsList = postsList.sort(function (a, b) {
+      return new Date(a.created) - new Date(b.created);
+    });
+  }
 
-    try{
-
-        
-        for (let i = 0; i < json.length; i++) {
-            const date = new Date (json[i].created);
-            const created = date.toDateString(); 
-            posts.innerHTML += `<a href="details.html?id=${json[i].id}" <div class=" container mt-4 mb-5 posts-card d-flex justify-content-center row col-md-8 feed p-2
+  try {
+    posts.innerHTML = '';
+    for (let i = 0; i < postsList.length; i++) {
+      const date = new Date(postsList[i].created);
+      const created = date.toDateString();
+      posts.innerHTML += `<a href="details.html?id=${postsList[i].id}" <div class=" container mt-4 mb-5 posts-card d-flex justify-content-center row col-md-8 feed p-2
             bg-white border mt-2">
             <div class="d-flex flex-row justify-content-between align-items-center p-2 border-bottom">
-            <p class="d-flex justify-content-end socials">${json[i].author.name}</p></div>
+            <p class="d-flex justify-content-end socials">${postsList[i].author.name}</p></div>
             <div class="d-flex flex-row justify-content-between align-items-center p-2 border-bottom">
-            <h1 class="d-flex flex-row align-items-center feed-text px-2">${json[i].title}</h1></div>
+            <h1 class="d-flex flex-row align-items-center feed-text px-2">${postsList[i].title}</h1></div>
             <div class="feed-image p-2 px-3">
-            <img class="img-fluid img-responsive d-flex justify-content-end socials" src ="${json[i].media}"></img></div>
+            </div>
             <div class="p-2 px-3">
-            <p class="d-flex justify-content-end socials">${json[i].body}</p></div>
+            <p class="d-flex justify-content-end socials">${postsList[i].body}</p></div>
             <div><p>${created}</p></div>
-            <button class="btn btn-primary waves-effect waves-light" >View Post</button></a>`
-        
-         }
-
-      
-
-    } catch (error) {
-    console.log(error)
+            <button class="btn btn-primary waves-effect waves-light" >View Post</button></a>`;
     }
-
-
-
+  } catch (error) {
+    console.log(error);
+  }
 }
 
 renderPosts();
-
-oldest.addEventListener("click", filterNewest);
-newest.addEventListener("click", filterOldest);
- 
-
 
